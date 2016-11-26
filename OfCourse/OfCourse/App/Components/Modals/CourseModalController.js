11cf0b7f5c5b4b1d8a87e3e3017885dd2f788566
Jackie Robinson <jackierobinsonm@gmail.com>
2016-11-26 15:08:00 -0600

    fixed modal

--- OfCourse/OfCourse/App/Components/Modals/CourseModalController.js
+++ OfCourse/OfCourse/App/Components/Modals/CourseModalController.js
@@ -1,26 +1,53 @@
 ﻿var app = angular.module('ofCourse');
 
-app.controller('CourseModalController', function ($uibModal, $log) {
+app.controller('CourseModalController', function ($scope, $uibModal, $log) {
+
     var $ctrl = this;
-    $ctrl.items = ['item1', 'item2', 'item3'];
+
+    $scope.courses =
+  [
+  {
+      "department": "CSC",
+      "number": 3102,
+      "name": "Advanced Data Structures and Algorithm Analysis",
+      "location": "Tureaud Hall",
+      "time": "1:30-2:50 TTH",
+      "professor": "Kooima R",
+      "description": "Prereq.: CSC 1254 or CSC 1351 and credit or concurrent enrollment in CSC 2259 or EE 2740. Description and utilization of formal ADT representations, especially those on lists, sets and graphs; time and space analysis of recursive and nonrecursive algorithms, including graph and sorting algorithms; algorithm design techniques."
+  },
+  {
+      "department": "CSC",
+      "number": 1200,
+      "name": "Ethics in Computing",
+      "location": "Tureaud Hall",
+      "time": "10:30-11:20 W",
+      "professor": "Busch K",
+      "description": "Prereq.: CSC 1254 or CSC 1351 and credit or concurrent enrollment in CSC 2259 or EE 2740. Description and utilization of formal ADT representations, especially those on lists, sets and graphs; time and space analysis of recursive and nonrecursive algorithms, including graph and sorting algorithms; algorithm design techniques."
+  },
+  {
+      "department": "CSC",
+      "number": 4330,
+      "name": "Software Systems Development",
+      "location": "Tureaud Hall",
+      "time": "5:00-6:30 MW",
+      "professor": "Mahmoud A",
+      "description": "Prereq.: CSC 1254 or CSC 1351 and credit or concurrent enrollment in CSC 2259 or EE 2740. Description and utilization of formal ADT representations, especially those on lists, sets and graphs; time and space analysis of recursive and nonrecursive algorithms, including graph and sorting algorithms; algorithm design techniques."
+  }
+  ];
 
     $ctrl.animationsEnabled = true;
 
-    $ctrl.open = function (size, parentSelector) {
-        var parentElem = parentSelector ?
-          angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+    $ctrl.viewCourse = function (size, selectedCourse) {
+      
         var modalInstance = $uibModal.open({
             animation: $ctrl.animationsEnabled,
-            ariaLabelledBy: 'modal-title',
-            ariaDescribedBy: 'modal-body',
-            templateUrl: 'myModalContent.html',
+            templateUrl: '/App/Components/Modals/CourseModalContent.html',
             controller: 'ModalInstanceCtrl',
             controllerAs: '$ctrl',
             size: size,
-            appendTo: parentElem,
             resolve: {
-                items: function () {
-                    return $ctrl.items;
+                course: function () {
+                    return selectedCourse;
                 }
             }
         });
